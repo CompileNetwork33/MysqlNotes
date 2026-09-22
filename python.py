bf208f10f19eb1646aa438c6dfd7537ec15c15fe
CompileNetwork33 <vaishnavimishra567567@gmail.com>
2026-09-22 10:11:38 +0530

    introduction of sql and why is used in data analyst

---
+++ python.py
@@ -0,0 +1,8 @@
+import sqlite3
+conn = sqlite3.connect("test.db")   # same db jo VS Code me hai
+cursor = conn.cursor()
+
+import pandas as pd
+print(pd.__version__)
+import matplotlib.pyplot as plt
+
